24104de8d5f30fa26cbc156aa65c4a9976779f60
lbrealdev <lbrealdeveloper@gmail.com>
2025-07-16 11:54:18 +0200

    test(unsigned): decode transactions #01

--- src/rawtxdecode_py/decode.py
+++ src/rawtxdecode_py/decode.py
@@ -2,6 +2,8 @@ from eth.vm.forks.arrow_glacier.transactions import (
     ArrowGlacierTransactionBuilder as TransactionBuilder,
 )
 from eth_utils import to_bytes
+from eth_account.typed_transactions import TypedTransaction
+from hexbytes import HexBytes
 
 
 class DecodedTransactionWrapper:
@@ -31,3 +33,41 @@ def decode_raw_transaction(tx: str) -> DecodedTransactionWrapper:
     tx_bytes = convert_hex_to_bytes(tx)
     decoded_tx = TransactionBuilder().decode(tx_bytes)
     return DecodedTransactionWrapper(decoded_tx)
+
+
+def decode_unsigned_raw_transaction(tx: str) -> DecodedTransactionWrapper:
+    """
+    :param tx: Raw Ethereum transaction in bytes format.
+    :return: DecodedTransactionWrapper object providing
+             both attribute-style access (e.g., .to, .sender)
+             and dictionary-style access to transaction fields.
+    """
+    tx_bytes = HexBytes(convert_hex_to_bytes(tx))
+    decode_tx = TransactionBuilder().decode(tx_bytes)
+
+    return DecodedTransactionWrapper(decode_tx)
+
+
+def decode_raw_transaction_test(tx: str) -> DecodedTransactionWrapper:
+    """
+    :param tx: Raw Ethereum transaction in bytes format.
+    :return: DecodedTransactionWrapper object providing
+             both attribute-style access (e.g., .to, .sender)
+             and dictionary-style access to transaction fields.
+    """
+    tx_bytes = convert_hex_to_bytes(tx)
+    decoded_tx = TransactionBuilder().decode(tx_bytes)
+    return DecodedTransactionWrapper(decoded_tx)
+
+
+def decode_raw_transaction_typed(tx: str) -> DecodedTransactionWrapper:
+    """
+    Function to decode unsigned transaction (test)
+    :param tx: Raw Ethereum transaction in bytes format.
+    :return: DecodedTransactionWrapper object providing
+             both attribute-style access (e.g., .to, .sender)
+             and dictionary-style access to transaction fields.
+    """
+    tx_bytes = HexBytes(convert_hex_to_bytes(tx))    
+    tx_typed = TypedTransaction.from_bytes(tx_bytes)
+    return DecodedTransactionWrapper(tx_typed)
--- unsigned/main.py
+++ unsigned/main.py
@@ -1,5 +1,11 @@
 import sys
-from rawtxdecode_py.decode import decode_raw_transaction
+from rawtxdecode_py.decode import (
+  decode_raw_transaction, 
+  decode_unsigned_raw_transaction, 
+  decode_raw_transaction_test,
+  decode_raw_transaction_typed
+)
+import json
 
 
 def main():
@@ -9,14 +15,59 @@ def main():
         print("Usage: main.py <raw-transaction-hash>")
         sys.exit(1)
 
-    decode_raw_tx = decode_raw_transaction(raw_tx_hex)
-    tx_fields = decode_raw_tx.fields["_inner"].as_dict()
+
+
+    # test unsigned transaction #01
+    print()
+    print("#01 - Default with HexBytes")
+    print()
+
+    decode_unsigned_raw_tx_1 = decode_unsigned_raw_transaction(raw_tx_hex)
+    # print(dir(decode_unsigned_raw_tx_1))
+
+    # Debug transaction fields #01
+    tx_fields_1 = decode_unsigned_raw_tx_1.fields["_inner"].as_dict()
+    for key, value in tx_fields_1.items():
+        print(f"key: {key} - value: {value}")
+    
+
+
+    # test unsigned transaction #02
+    print()
+    print("#02 - Default")
+    print()
+
+    decode_unsigned_raw_tx_2 = decode_raw_transaction_test(raw_tx_hex)
+    # print(dir(decode_unsigned_raw_tx_2))
+
+    tx_fields_2 = decode_unsigned_raw_tx_2.fields["_inner"].as_dict()
+    for key, value in tx_fields_2.items():
+        print(f"key: {key} - value: {value}")
+
+    
+
+    # test unsigned transaction #03
+    print()
+    print("#03 - TypedTransaction (unsigned/signed)")
+    print()
+    
+    decode_unsigned_raw_tx_3 = decode_raw_transaction_typed(raw_tx_hex)
+    # print(dir(decode_unsigned_raw_tx_3))
+    
+    # TypedTransaction don't has _inner
+    tx_fields_3 = decode_unsigned_raw_tx_3.fields["transaction"].as_dict()
+    for key, value in tx_fields_3.items():
+        print(f"key: {key} - value: {value}")
+
+
+
+
+    # tx_fields = decode_raw_tx.
+
+    # decode_raw_tx = decode_raw_transaction(raw_tx_hex)
+    # tx_fields = decode_raw_tx.fields["_inner"].as_dict()
 
     # print(dir(decode_raw_tx))
-
-    # Debug transaction fields
-    for key, value in tx_fields.items():
-        print(f"key: {key} - value: {value}")
 
 
 if __name__ == "__main__":
