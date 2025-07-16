e92a1bbb2ac959c6c440c7f312e2c35ffc3a4cba
lbrealdev <lbrealdeveloper@gmail.com>
2025-07-16 10:51:18 +0200

    test(unsigned): debug transaction fields

--- unsigned/main.py
+++ unsigned/main.py
@@ -1,15 +1,23 @@
 import sys
+from rawtxdecode_py.decode import decode_raw_transaction
 
 
 def main():
-    args = sys.argv[1]
+    raw_tx_hex = sys.argv[1]
 
-    if not args:
+    if not raw_tx_hex:
         print("Usage: main.py <raw-transaction-hash>")
         sys.exit(1)
-    
-    print(args)
+
+    decode_raw_tx = decode_raw_transaction(raw_tx_hex)
+    tx_fields = decode_raw_tx.fields["_inner"].as_dict()
+
+    # print(dir(decode_raw_tx))
+
+    # Debug transaction fields
+    for key, value in tx_fields.items():
+        print(f"key: {key} - value: {value}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
